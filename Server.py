d0f2180ac304e4c737054fe033de087d5f6c64b4
bpthien14 <bpthien21@clc.fitus.edu.vn>
2022-12-23 21:40:28 +0700

    Code retro
    Day la code trc khi duoc decorate vi sau khi decorate thi no dinh bug

--- Server.py
+++ Server.py
@@ -1,90 +1,161 @@
 import socket
 import threading
 from os import error
+import string
 import webbrowser
 
 HOST = '127.0.0.1'
 PORT = 8080
 FORMAT = "utf8"
-FILE_SIZE =1024
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+LOGIN = "login"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-    server.bind((HOST, PORT))
-    print('* Running on http://127.0.0.1:8080')
+    s.bind((HOST, PORT))
+
+    print("SERVER SIDE")
+    print('* Running on http://{HOST}:{PORT}')
+    print("server:", HOST, PORT)
     print("Waiting for Client!!!")
 
 except socket.error as e:
     print('socket error: {e}')
     print('socket error: %s' %(e)) 
 
-def html():
-  return 'text/html'
 
-def txt():
-  return 'text/plain'
 
-def jpg():
-  return 'image/jpeg'
 
-def png():
-  return 'image/png'
-
-def css():
-  return 'text/css'
-
-def cont_type(x):
-    switcher = {
-        'html': html(),
-        'txt': txt(),
-        'jpg': jpg(),
-        'jpeg': jpg(),
-        'css': css()
-    }
-    return switcher.get(x, "nothing")
-
-def readRequest(conn):
-    request = conn.recv(FILE_SIZE).decode()
-    print(request)
-    if not request: print("Didn't receive request!!")
-    return request
 
 def handleClient(conn, addr):
     while True:
-        data = readRequest(conn)
-
+        data = conn.recv(1024).decode()
+        print(data)
+        if not data: break
         request_line = data.split('\r\r')[0]
-        
+        print('1 ', request_line)
         request_method = request_line.split(' ')[0]
-        
+        print('2 ', request_method)
         request_url = (request_line.split(' ')[1]).strip('/')    
-        url_tail = request_url.split('.')[1]
-        
+        print('3 ', request_method, request_url)
         length = len(request_line.split('\n'))
-
-        user = request_line.split('\r\n')[length - 1]
+        print(length)
+        x = request_line.split('\r\n')[length - 1]
         
         if request_method == 'GET':
-            Content_type = cont_type(url_tail)
-            if url_tail != 'ico':
-                data = read_file(request_url,Content_type)
+            if request_url == '' or request_url == 'index.html':
+                #trang chu
+                url = 'index.html'
+                Content_type = 'text/html'
+                data = read_file(url, Content_type)
                 conn.send(data)
+                del data
+                pass
+            elif request_url == 'utils.css':
+                #trang chu
+                url = 'utils.css'
+                Content_type = 'text/css'
+                data = read_file(url, Content_type)
+                conn.send(data)
+                del data
+                pass
+            
+            elif request_url == 'images/background.png':
+                #trang chu
+                url = 'images/background.png'
+                Content_type = 'image/png'
+                data = read_file(url, Content_type)
+                conn.send(data)
+                del data
+                pass
+            elif request_url == 'images/tetviet.png':
+                url = 'images/tetviet.png'
+                Content_type = 'image/png'
+                data = read_file(url, Content_type)
+                conn.send(data)
+                del data
+                pass
+                
+            elif request_url == 'images/bg.png':
+                #trang chu
+                url = 'images/bg.png'
+                Content_type = 'image/png'
+                data = read_file(url, Content_type)
+                conn.send(data)
+                del data
+                pass
+            elif request_url == 'images/LogoFIT.png':
+                url = 'images/LogoFIT.png'
+                Content_type = 'image/png'
+                data = read_file(url, Content_type)
+                conn.send(data)
+                del data
+                pass
+            elif request_url == 'images/1.jpg':
+                #trang chu
+                url = 'images/1.jpg'
+                Content_type = 'image/jpeg'
+                data = read_file(url, Content_type)
+                conn.send(data)
+                del data
+                pass
+            
+            elif request_url == 'images/2.jpg':
+                #trang chu
+                url = 'images/2.jpg'
+                Content_type = 'image/jpeg'
+                data = read_file(url, Content_type)
+                conn.send(data)
+                del data
+                pass
+            
+            elif request_url == 'images/3.jpg':
+                #trang chu
+                url = 'images/3.jpg'
+                Content_type = 'image/jpeg'
+                data = read_file(url, Content_type)
+                conn.send(data)
+                del data
+                pass
+            
+            elif request_url == 'images/4.jpg':
+                #trang chu
+                url = 'images/4.jpg'
+                Content_type = 'image/jpeg'
+                data = read_file(url, Content_type)
+                conn.send(data)
+                del data
+                pass
+            
+            else: 
+                url = '404.html'
+                Content_type = 'text/html'
+                data = read_file(url, Content_type)
+                conn.send(data)
+        
         elif request_method == 'POST':
-            if request_url == 'images.html' and user == 'Username=admin&Password=123456':
-                Content_type = 'text/html' 
-                data = read_file(request_url, Content_type)
+            if request_url == 'images.html' and x == 'Username=admin&Password=123456':
+                url = 'images.html'
+                Content_type = 'text/html'
+                data = read_file(url, Content_type)
                 conn.send(data)
+
             else:
                 url = '401.html'
                 Content_type = 'text/html'   
                 data = read_file(url, Content_type)
                 conn.send(data)
+            
+                
+            
+        
         else:
             url = '404.html'
             Content_type = 'text/html'   
             data = read_file(url, Content_type)
             conn.send(data)
+            
         conn.close()   
         break
 
@@ -101,16 +172,18 @@ def reponse_header(Content_type):
     mess_head += '\r\n\r\n'
     mess_head = mess_head.encode()
     return mess_head
+    
+    
+def serverLogin(conn):
+    client_account = conn.recv()
 
-def start():
-    server.listen()
+    
+if __name__ == '__main__':
+    s.listen()
     while True:
         try:
-            conn, addr = server.accept()
+            conn, addr = s.accept()
             thread = threading.Thread(target=handleClient, args=(conn, addr))
             thread.start()
         except:
             print('Error')
-
-if __name__ == '__main__':
-    start()
