7fe616e44a09106985a55fcce54624557ad9077f
quancyber <quankngl01@gmail.com>
2022-12-25 11:01:27 +0700

    review code

--- Server.py
+++ Server.py
@@ -6,7 +6,7 @@ import webbrowser
 HOST = '127.0.0.1'
 PORT = 8080
 FORMAT = "utf8"
-FILE_SIZE =1024
+FILE_SIZE = 1024
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -17,8 +17,9 @@ try:
 
 except socket.error as e:
     print('socket error: {e}')
-    print('socket error: %s' %(e))
-    
+    print('socket error: %s' % (e))
+
+
 def start():
     server.listen()
     while True:
@@ -29,24 +30,34 @@ def start():
         except:
             print('Error')
 
+
 def readRequest(client):
     request = client.recv(FILE_SIZE).decode()
     print(request)
-    if not request: print("Didn't receive request!!")
+    if not request:
+        print("Didn't receive request!!")
     return request
 
+
 def html():
-  return 'text/html'
+    return 'text/html'
+
 
 def txt():
-  return 'text/plain'
+    return 'text/plain'
+
 
 def jpg():
-  return 'image/jpeg'
+    return 'image/jpeg'
+
+
 def png():
-  return 'image/png'
+    return 'image/png'
+
+
 def css():
-  return 'text/css'
+    return 'text/css'
+
 
 def cont_type(x):
     switcher = {
@@ -57,38 +68,41 @@ def cont_type(x):
         'css': css()
     }
     return switcher.get(x, "nothing")
+
+
 def handleClient(client, addr):
     while True:
         data = readRequest(client)
 
         request_line = data.split('\r\r')[0]
-        
+
         method = request_line.split(' ')[0]
-        
-        url = (request_line.split(' ')[1]).strip('/')    
-        
+
+        url = (request_line.split(' ')[1]).strip('/')
+
         length = len(request_line.split('\n'))
 
         user = request_line.split('\r\n')[length - 1]
-        
+
         if method == 'GET':
             if url == '' or url == 'index.html':
-                #trang chu
+                # trang chu
+                url = 'index.html'
                 Content_type = 'text/html'
                 data = read_file(url, Content_type)
                 client.send(data)
                 del data
                 pass
             elif url == 'utils.css':
-                #trang chu
+                # trang chu
+                url = 'utils.css'
                 Content_type = 'text/css'
                 data = read_file(url, Content_type)
                 client.send(data)
                 del data
                 pass
-            
             elif url == 'images/background.png':
-                #trang chu
+                # trang chu
                 Content_type = 'image/png'
                 data = read_file(url, Content_type)
                 client.send(data)
@@ -100,9 +114,9 @@ def handleClient(client, addr):
                 client.send(data)
                 del data
                 pass
-                
+
             elif url == 'images/bg.png':
-                #trang chu
+                # trang chu
                 Content_type = 'image/png'
                 data = read_file(url, Content_type)
                 client.send(data)
@@ -115,47 +129,55 @@ def handleClient(client, addr):
                 del data
                 pass
             elif url == 'images/1.jpg':
-                #trang chu
+                # trang chu
                 Content_type = 'image/jpeg'
                 data = read_file(url, Content_type)
                 client.send(data)
                 del data
                 pass
-            
+
             elif url == 'images/2.jpg':
-                #trang chu
+                # trang chu
                 Content_type = 'image/jpeg'
                 data = read_file(url, Content_type)
                 client.send(data)
                 del data
                 pass
-            
+
             elif url == 'images/3.jpg':
-                #trang chu
+                # trang chu
                 Content_type = 'image/jpeg'
                 data = read_file(url, Content_type)
                 client.send(data)
                 del data
                 pass
-            
+
             elif url == 'images/4.jpg':
-                #trang chu
+                # trang chu
                 Content_type = 'image/jpeg'
                 data = read_file(url, Content_type)
                 client.send(data)
                 del data
                 pass
-            elif url == 'favicon.io':   
+            elif url == 'favicon.io':
                 Content_type = 'image/jpeg'
                 data = read_file(url, Content_type)
                 client.send(data)
                 del data
                 pass
-            else: 
+            elif url == 'images/404Image3.png':
+                # 404
+                Content_type = 'image/png'
+                data = read_file(url, Content_type)
+                client.send(data)
+                del data
+                pass
+            else:
+                url = '404.html'
                 Content_type = 'text/html'
                 data = read_file(url, Content_type)
                 client.send(data)
-        
+
         elif method == 'POST':
             if url == 'images.html' and user == 'Username=admin&Password=123456':
                 Content_type = 'text/html'
@@ -163,24 +185,27 @@ def handleClient(client, addr):
                 client.send(data)
 
             else:
-                Content_type = 'text/html'   
+                url = '401.html'
+                Content_type = 'text/html'
                 data = read_file(url, Content_type)
                 client.send(data)
-            
+
         else:
             url = '404.html'
-            Content_type = 'text/html'   
+            Content_type = 'text/html'
             data = read_file(url, Content_type)
             client.send(data)
-            
-        client.close()   
+
+        client.close()
         break
+
 
 def read_file(filename, Content_type):
     f = open(filename, 'rb')
     fdata = reponse_header(Content_type)
     fdata += f.read()
     return fdata
+
 
 def reponse_header(Content_type):
     mess_head = 'HTTP/1.1 200 \n'
